de038ebecf9fce195d22a0df262433bada9080c3
bubersome <bubersome@gmail.com>
2022-08-01 09:42:48 -0500

    pointer practice

--- basic_content/pointer_refer/effec.cpp
+++ basic_content/pointer_refer/effec.cpp
@@ -14,9 +14,29 @@ void test2(int& p)
 
 int main() {
     int a=10;
-    int *p=&a;
+//    int *p=&a;
+    int* p=&a;
+    cout<<*p<<endl; //10
+    cout<<"*p"<<endl;
+    cout<<p<<endl;  //0x16da975f8
+    cout<<"p"<<endl;
+    cout<<&p<<endl; //0x16da975f0
+    cout<<"&p"<<endl;
+
+    cout<<*&p<<endl; //0x16da975f8
+    cout<<"*&p"<<endl;
+//    cout<<&0x16ce975f8<<endl; //error: cannot take the address of an rvalue of type 'long'
+
+    cout<< " "<<endl;
+    cout<<&a<<endl;
+    cout<<a<<endl;
     test1(p);
     test2(a);
     cout<<a<<endl;
+
+    int b =11;
+//    int* p2 =b; //Cannot initialize a variable of type 'int *' with an lvalue of type 'int'
+
+
     return 0;
 }
